37ad1c6766191d4497da365c9283b7b0f5aec693
formallyknownasdave <david.gaul@botcorp.com>
2019-06-09 23:31:56 -0500

    Test fix for servers with no STARTTLS support

--- frappe/email/smtp.py
+++ frappe/email/smtp.py
@@ -201,9 +201,10 @@ class SMTPServer:
 		try:
 			if self.use_tls and not self.port:
 				self.port = 587
-
-			self._sess = smtplib.SMTP((self.server or "").encode('utf-8'),
-				cint(self.port) or None)
+			if self.port is 465:
+				self._sess = smtplib.SMTP_SSL((self.server or "").encode('utf-8'),cint(self.port) or None)
+			else:
+				self._sess = smtplib.SMTP((self.server or "").encode('utf-8'),cint(self.port) or None)
 
 			if not self._sess:
 				err_msg = _('Could not connect to outgoing email server')
